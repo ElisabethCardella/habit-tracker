9c54d40fb04842d100ff99b2b85718513535b1b5
Marcin Gardas <mkgardas@gmail.com>
2022-01-09 01:25:16 +0000

    Set up

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import { Button } from "@chakra-ui/react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Button colorScheme="blue">Les restaurants près de moi</Button>{" "}
+    </div>
+  );
+};
+
+export default Header;
