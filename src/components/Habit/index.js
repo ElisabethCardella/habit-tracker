80e7db765059248507cea173d650ab68d7b3879a
ElisabethCardella <elisabeth.cardellav@gmail.com>
2022-01-17 23:17:04 +0100

    Build habit list UI

--- src/components/Habit/index.js
+++ src/components/Habit/index.js
@@ -2,10 +2,16 @@ import { Container, HStack, Spacer } from "@chakra-ui/react";
 import { Editable, EditableInput, EditablePreview } from "@chakra-ui/react";
 import { Checkbox, Stack, VStack, Text } from "@chakra-ui/react";
 import { Divider } from "@chakra-ui/react";
-import { Grid, GridItem } from "@chakra-ui/react";
+import {
+  List,
+  ListItem,
+  ListIcon,
+  OrderedList,
+  UnorderedList,
+} from "@chakra-ui/react";
 
 const Habit = () => {
-  const Habits = [
+  /*const eachHabit = [
     {
       id: 1,
       body: "sleep",
@@ -15,11 +21,10 @@ const Habit = () => {
       id: 2,
       body: "dance",
     },
-  ];
+  ];*/
 
   return (
     <VStack
-      divider={<Divider />}
       borderColor="#97266D"
       border="2px"
       p="4"
@@ -28,17 +33,40 @@ const Habit = () => {
       maxW={{ base: "100%", md: "50%", xl: "25%" }}
       alignItems="stretch"
     >
-      {Habits.map((habit) => (
-        <HStack p={5} key={habit.id}>
-          <Editable>
-            {habit.body}
-            <EditablePreview />
-            <EditableInput />
-          </Editable>
-          <Spacer />
-          <Checkbox size="lg" />
-        </HStack>
-      ))}
+      <UnorderedList>
+        <ListItem>
+          <HStack p={5} /*key={eachHabit.id}*/>
+            <Editable>
+              Cinema
+              <EditableInput />
+            </Editable>
+            <Spacer />
+            <Checkbox size="lg" />
+          </HStack>
+        </ListItem>
+        <Divider />
+        <ListItem>
+          <HStack p={5} /*key={eachHabit.id}*/>
+            <Editable>
+              Exercices
+              <EditableInput />
+            </Editable>
+            <Spacer />
+            <Checkbox size="lg" />
+          </HStack>
+        </ListItem>
+        <Divider />
+        <ListItem>
+          <HStack p={5} /*key={eachHabit.id}*/>
+            <Editable>
+              Dancing
+              <EditableInput />
+            </Editable>
+            <Spacer />
+            <Checkbox size="lg" />
+          </HStack>
+        </ListItem>
+      </UnorderedList>
     </VStack>
   );
 };
