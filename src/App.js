1464c28916dee6fc35cbb976f976bb981a643e0e
ElisabethCardella <elisabeth.cardellav@gmail.com>
2022-01-18 23:56:49 +0100

    Changes epurate version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import "./App.css";
-import Header from "./components/Header";
 import Habit from "./components/Habit";
-import AddHabit from "./components/AddHabit";
 import {
   VStack,
   IconButton,
@@ -10,7 +8,6 @@ import {
 } from "@chakra-ui/react";
 import { FaSun, FaMoon } from "react-icons/fa";
 import { useState, useEffect } from "react";
-import Days from "./components/Days";
 
 function App(habits) {
   const initialHabits = [
@@ -30,23 +27,7 @@ function App(habits) {
     },
   ];
 
-  useState[habits] = useState(initialHabits);
-
-  return (
-    <VStack p={5}>
-      <IconButton
-        icon={<FaSun />}
-        isRound="true"
-        size="lg"
-        alignSelf="flex-end"
-      />
-      <Header />
-      <Days />
-
-      <Habit habits={habits} />
-      <AddHabit />
-    </VStack>
-  );
+  return <Habit />;
 }
 
 export default App;
--- src/components/Habit/index.js
+++ src/components/Habit/index.js
@@ -1,8 +1,11 @@
-import { HStack, Spacer } from "@chakra-ui/react";
+import { Box, Container, HStack, Spacer } from "@chakra-ui/react";
 import { Editable, EditableInput } from "@chakra-ui/react";
 import { Checkbox, VStack } from "@chakra-ui/react";
 import { Divider } from "@chakra-ui/react";
 import { ListItem, UnorderedList } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import { Flex } from "@chakra-ui/react";
 
 const Habit = () => {
   /*const eachHabit = [
@@ -18,50 +21,79 @@ const Habit = () => {
   ];*/
 
   return (
-    <VStack
-      borderColor="#97266D"
-      border="2px"
-      p="4"
-      borderRadius="lg"
-      w="100%"
-      maxW={{ base: "100%", md: "50%", xl: "25%" }}
-      alignItems="stretch"
-    >
-      <UnorderedList>
-        <ListItem>
-          <HStack p={5} /*key={eachHabit.id}*/>
-            <Editable>
-              Cinema
-              <EditableInput />
-            </Editable>
-            <Spacer />
-            <Checkbox size="lg" />
-          </HStack>
-        </ListItem>
-        <Divider />
-        <ListItem>
-          <HStack p={5} /*key={eachHabit.id}*/>
-            <Editable>
-              Exercices
-              <EditableInput />
-            </Editable>
-            <Spacer />
-            <Checkbox size="lg" />
-          </HStack>
-        </ListItem>
-        <Divider />
-        <ListItem>
-          <HStack p={5} /*key={eachHabit.id}*/>
-            <Editable>
-              Dancing
-              <EditableInput />
-            </Editable>
-            <Spacer />
-            <Checkbox size="lg" />
-          </HStack>
-        </ListItem>
-      </UnorderedList>
-    </VStack>
+    <Container>
+      <Container
+        borderColor="#97266D"
+        border="2px"
+        p="4"
+        w="100%"
+        maxW={{ base: "100%", md: "100%", xl: "100%" }}
+      >
+        <Flex>
+          <Box>
+            <button>
+              <ChevronLeftIcon />
+            </button>
+          </Box>
+          <Spacer />
+          <Box>
+            <Text>Monday</Text>
+          </Box>
+          <Spacer />
+          <Box>
+            <button>
+              <ChevronRightIcon />
+            </button>
+          </Box>
+        </Flex>
+      </Container>
+
+      <Container>
+        <VStack p="4" w="100%">
+          <UnorderedList>
+            <ListItem>
+              <HStack p={5} /*key={eachHabit.id}*/>
+                <Editable>
+                  Cinema
+                  <EditableInput />
+                </Editable>
+                <Spacer />
+                <Checkbox size="lg" />
+              </HStack>
+            </ListItem>
+            <Divider />
+            <ListItem>
+              <HStack
+                spacing="150"
+                p={5}
+                display="flex"
+                justifyContent="stretch"
+                /*key={eachHabit.id}*/
+              >
+                <Editable>
+                  Exercices
+                  <EditableInput />
+                </Editable>
+                <Spacer />
+
+                <Checkbox size="lg" />
+              </HStack>
+            </ListItem>
+            <Divider />
+            <ListItem>
+              <HStack p={5} /*key={eachHabit.id}*/>
+                <Editable>
+                  Dancing
+                  <EditableInput />
+                </Editable>
+                <Spacer />
+                <Checkbox size="lg" />
+              </HStack>
+            </ListItem>
+          </UnorderedList>
+        </VStack>
+      </Container>
+    </Container>
   );
 };
 
